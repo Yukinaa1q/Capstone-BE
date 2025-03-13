e929cd62d54e3dacbd0806ab05c83b0b95e745ea
Phuc <phuc.nguyenngocquang@hcmut.edu.vn>
2025-03-13 15:58:51 +0700

    Edited room module DTOs

--- src/modules/room/dto/createRoom.dto.ts
+++ src/modules/room/dto/createRoom.dto.ts
@@ -1,36 +1,16 @@
 import { OmitUpdateType } from '@services/openApi';
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import {
-  IsArray,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 import { Room } from '@modules/courseRegistration/entity/room.entity';
 
 export class CreateRoomDTO extends OmitUpdateType(Room, ['roomId']) {
-  @ApiPropertyOptional()
+  @IsBoolean()
+  isOnline: boolean;
+
   @IsString()
   @IsOptional()
   roomCode?: string;
 
-  @ApiPropertyOptional()
   @IsString()
   @IsOptional()
   roomAdress?: string;
-
-  @ApiProperty()
-  @IsNumber()
-  @IsNotEmpty()
-  maxClasses: number;
-
-  @ApiPropertyOptional()
-  @IsString()
-  @IsOptional()
-  onlineRoom?: string;
-
-  @IsArray()
-  @IsOptional()
-  classIdList?: string[];
 }
