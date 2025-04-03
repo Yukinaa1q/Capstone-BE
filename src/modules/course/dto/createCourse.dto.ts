743f76ff592b47fbb85e118c5989c281d19193fd
Phuc <phuc.nguyenngocquang@hcmut.edu.vn>
2025-04-03 14:39:59 +0700

    Modified Course Entity and added courseDuration

--- src/modules/course/dto/createCourse.dto.ts
+++ src/modules/course/dto/createCourse.dto.ts
@@ -1,8 +1,11 @@
 import { OmitUpdateType } from '@services/openApi';
-import { IsString } from 'class-validator';
+import { IsString, IsNumber } from 'class-validator';
 import { Course } from '../entity/course.entity';
 
 export class CreateCourseDTO extends OmitUpdateType(Course, ['courseId']) {
   @IsString()
   courseCode: string;
+
+  @IsNumber()
+  courseDuration: number;
 }
